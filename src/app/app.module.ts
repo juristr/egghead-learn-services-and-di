121acc49bbbfbc138ab152d64b0a865fd0c8ffac
Juri <juri.strumpflohner@gmail.com>
2018-01-09 23:26:52 +0100

    lesson setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoggerService } from './services/logger.service';
-import { NewLoggerService } from './services/new-logger.service';
 import { PersonComponent } from './person.component';
 
 @NgModule({
   declarations: [AppComponent, PersonComponent],
   imports: [BrowserModule],
-  providers: [
-    NewLoggerService,
-    { provide: LoggerService, useExisting: NewLoggerService }
-  ],
+  providers: [LoggerService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
