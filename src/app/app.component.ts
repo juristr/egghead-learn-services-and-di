4f52454051bc27353634819d1b7f355d33149dc6
Juri <juri.strumpflohner@gmail.com>
2018-01-09 22:28:52 +0100

    create people module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { PeopleService } from './people.service';
+import { PeopleService } from './people/people.service';
 
 @Component({
   selector: 'app-root',
