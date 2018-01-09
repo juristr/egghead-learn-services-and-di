70c8f58417cf5a0c971a88b774e71b056034240d
Juri <juri.strumpflohner@gmail.com>
2018-01-09 21:49:11 +0100

    initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Angular Services</h1>
+  `
 })
 export class AppComponent {
   title = 'app';
